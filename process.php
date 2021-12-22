04f3d4157a92b89b68cced43a746e2d4f63cf016
AFIQ AIMAN <afiqaiman091@gmail.com>
2021-12-22 23:56:20 +0800

    Revert "tambah cat column"
    This reverts commit 3150e6fbb104963a8690191c3a5d162e88695199.

--- process.php
+++ process.php
@@ -5,17 +5,15 @@ session_start();
 $mysqli = new mysqli('localhost', 'root', '','crud') or die(mysqli_error($mysqli));
 
 $name = '';
-$cname = '';
 $location = '';
 $update = false;
 $id=0;
 
 if(isset($_POST['save'])){
     $name = $_POST['name'];
-    $cname = $_POST['cname'];
     $location = $_POST['location'];
     
-    $mysqli->query("INSERT into data(name,catname,location) values('$name','$cname','$location')") or die($mysqli->error);
+    $mysqli->query("INSERT into data(name,location) values('$name','$location')") or die($mysqli->error);
 
     $_SESSION['message'] = "record has been saved!";
     $_SESSION['msg_type'] = "success";
@@ -42,7 +40,6 @@ if(isset($_GET['edit'])){
     if ($result->num_rows){
         $row = $result->fetch_array();
         $name = $row['name'];
-        $cname = $row['cname'];
         $location = $row['location'];
     }
 }
@@ -50,10 +47,9 @@ if(isset($_GET['edit'])){
 if (isset($_POST['update'])) {
     $id = $_POST['id'];
     $name = $_POST['name'];
-    $cname = $_POST['cname'];
     $location = $_POST['location'];
   
-    $result = $mysqli->query("UPDATE data SET name = '$name', catname = '$cname', location = '$location'  WHERE id = $id") or die($mysqli->error);
+    $result = $mysqli->query("UPDATE data SET name = '$name', location = '$location'  WHERE id = $id") or die($mysqli->error);
   
     $_SESSION['message'] = "Record has been updated";
     $_SESSION['msg_type'] = "warning";
